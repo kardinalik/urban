b6df4bdff962d1e21ffa459130d21f15a813a518
kardinalik <pvt711@mail.ru>
2024-09-05 21:26:56 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Введите ваше ФИО: ')
+print(my_string)
+print(my_string.upper())
+print(my_string.capitalize())
+print(my_string.replace(' ', ''))
+print(my_string[:1])
+print(my_string[-1])
