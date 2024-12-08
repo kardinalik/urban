bfb7215e49a0cb29dbf03e8e12c21538ba8f1981
kardinalik <pvt711@mail.ru>
2024-12-08 07:30:10 +0300

    Add files via upload

--- module_9.5.py
+++ module_9.5.py
@@ -39,13 +39,13 @@ iter5 = Iterator(10, 1)
 
 for i in iter2:
     print(i, end=' ')
-    print()
+print()
 for i in iter3:
     print(i, end=' ')
-    print()
+print()
 for i in iter4:
     print(i, end=' ')
-    print()
+print()
 for i in iter5:
     print(i, end=' ')
-    print()
+print()
